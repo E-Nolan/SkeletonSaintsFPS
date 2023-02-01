c8a70350568fa6d3e7671e7e399d8589016ca63a
Minish <EENolan@student.fullsail.edu>
2023-02-01 01:07:12 -0500

    playerController Jump
    Added jump functionality to playerController and added a test scene for player controls

--- skeleton-saints-ahi/Assets/Scripts/playerController.cs
+++ skeleton-saints-ahi/Assets/Scripts/playerController.cs
@@ -5,16 +5,27 @@ using UnityEngine;
 public class playerController : MonoBehaviour
 {
 
+    // Components used by this script
+    [Header("-=- Components -=-")]
     [SerializeField] CharacterController controller;
 
-    [SerializeField] int playerSpeed;
-    [SerializeField] int jumpTimes;
-    [SerializeField] int jumpSpeed;
-    [SerializeField] int gravity;
+    // Stats that determine player movement
+    [Header("Player Stats")]
+    [Range(1, 30)] [SerializeField] int playerSpeed;
+    [Range(1, 5)]  [SerializeField] int maxJumps;
+    [Range(3, 50)] [SerializeField] int jumpSpeed;
+    [Range(0, 50)] [SerializeField] int gravity;
 
+    // Stats used by the player's gun
+    [Header("Weapon Stats")]
 
-    Vector3 move;
-    Vector3 playerVelocity;
+    // Debug bool. Enable to receive Debug messages in the console
+    [SerializeField] bool debugMessages;
+
+    // Private variables used within the script to facilitate movement and actions
+    private Vector3 moveInput;
+    private Vector3 playerVelocity;
+    private int jumpsCurrent = 0;
 
     // Start is called before the first frame update
     void Start()
@@ -28,11 +39,32 @@ public class playerController : MonoBehaviour
         movement();
     }
 
+    // Tell the player where to move based on player input
     void movement()
     {
-        move = (transform.right * Input.GetAxis("Horizontal") + 
+        // Set the player's vertical velocity to 0 if they are standing on ground
+        if (controller.isGrounded && playerVelocity.y <= 0)
+        {
+            playerVelocity.y = 0;
+            jumpsCurrent = 0;
+        }
+
+        // Move the character via arrow keys/WASD input
+        moveInput = (transform.right * Input.GetAxis("Horizontal") + 
                 transform.forward * Input.GetAxis("Vertical"));
 
-        controller.Move(move * Time.deltaTime * playerSpeed);
+        controller.Move(moveInput * Time.deltaTime * playerSpeed);
+
+        // Allow the player to jump if they haven't exceeded their maximum amount of jumps
+        // TODO: Prevent the player from being able to perform a mid-air jump after falling off a ledge
+        if (Input.GetButtonDown("Jump") && jumpsCurrent < maxJumps)
+        {
+            jumpsCurrent++;
+            playerVelocity.y = jumpSpeed;
+        }
+
+        // Accelerate the player downward via gravity
+        playerVelocity.y -= (gravity * Time.deltaTime);
+        controller.Move(playerVelocity * Time.deltaTime);
     }
 }
