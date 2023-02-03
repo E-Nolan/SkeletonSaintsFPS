14712734f82e54c2a30fad0701577380ab5ac477
LJFisher1 <ljfisher1@student.fullsail.edu>
2023-02-03 14:00:13 -0600

    Added bullet prefab

--- skeleton-saints-ahi/Assets/Scripts/bullet.cs
+++ skeleton-saints-ahi/Assets/Scripts/bullet.cs
@@ -6,8 +6,11 @@ public class bullet : MonoBehaviour
 {
     // Dmg for the bullet
     public int bulletDmg;
+
     // Timer for how long the before the bullet is destroyed if no hit occurs
     [SerializeField] int timer;
+
+
 
    
     void Start()
