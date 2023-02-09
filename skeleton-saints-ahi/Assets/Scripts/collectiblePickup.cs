1405b4594e6f61cf1f36d30f612db3751ba7481f
Minish <EENolan@student.fullsail.edu>
2023-02-08 21:57:40 -0500

    Health and Ammo Pickups

---
+++ skeleton-saints-ahi/Assets/Scripts/collectiblePickup.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class collectiblePickup : MonoBehaviour
+{
+    [Range(0,100)] [SerializeField] int ammoRecovery;
+    [Range(0,100)] [SerializeField] int healthRecovery;
+    [Tooltip("Positive = Clockwise | Negative = Counter-Clockwise")]
+    [Range(-360, 360)] [SerializeField] int rotationSpeed;
+    [SerializeField] AudioClip pickupSound;
+
+    private void Update()
+    {
+        transform.Rotate(transform.up * rotationSpeed * Time.deltaTime);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            // The pickup can only be collected if the respective resource isn't already full
+            if ((ammoRecovery > 0 && !gameManager.instance.playerScript.isAmmoFull()) || (healthRecovery > 0 && !gameManager.instance.playerScript.isHealthFull()))
+            {
+                gameManager.instance.playerScript.giveAmmo(ammoRecovery);
+                gameManager.instance.playerScript.GiveHealth(healthRecovery);
+
+                Destroy(gameObject);
+            }
+        }
+    }
+}
