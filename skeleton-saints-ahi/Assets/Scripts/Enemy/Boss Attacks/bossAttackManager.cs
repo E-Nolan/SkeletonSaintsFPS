47d405e980ff2927534ddbee447b019a618ad4c8
Minish <EENolan@student.fullsail.edu>
2023-03-06 22:36:55 -0500

    boss attack manager + wave

---
+++ skeleton-saints-ahi/Assets/Scripts/Enemy/Boss Attacks/bossAttackManager.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class bossAttackManager : MonoBehaviour
+{
+    [SerializeField] energyWave waveAttack;
+    [SerializeField] float waveCooldown;
+
+    float currWaveTimer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
