f065610e66ba91be364291ddbddc466f467d912d
Minish <EENolan@student.fullsail.edu>
2023-03-15 19:31:50 -0400

    Update IKey.cs

--- skeleton-saints-ahi/Assets/Scripts/Objects/IKey.cs
+++ skeleton-saints-ahi/Assets/Scripts/Objects/IKey.cs
@@ -53,21 +53,21 @@ public class IKey : collectionItem
                 Destroy(gameObject);
                 hUDManager.instance.card01.color = new Color(hUDManager.instance.card01.color.r, hUDManager.instance.card01.color.g, hUDManager.instance.card01.color.b, 1f);
                 hUDManager.instance.keyCard01Text.color = new Color(hUDManager.instance.keyCard01Text.color.r, hUDManager.instance.keyCard01Text.color.g, hUDManager.instance.keyCard01Text.color.b, 1f);
-                gameManager.instance.keyCard[0] = !gameManager.instance.keyCard[0];
+                gameManager.instance.keyCard[0] = true;
             }
             else if (entryLevel[1])
             {
                 Destroy(gameObject);
                 hUDManager.instance.card02.color = new Color(hUDManager.instance.card02.color.r, hUDManager.instance.card02.color.g, hUDManager.instance.card02.color.b, 1f);
                 hUDManager.instance.keyCard02Text.color = new Color(hUDManager.instance.keyCard02Text.color.r, hUDManager.instance.keyCard02Text.color.g, hUDManager.instance.keyCard02Text.color.b, 1f);
-                gameManager.instance.keyCard[1] = !gameManager.instance.keyCard[1];
+                gameManager.instance.keyCard[1] = true;
             }
             else
             {
                 Destroy(gameObject);
                 hUDManager.instance.card03.color = new Color(hUDManager.instance.card03.color.r, hUDManager.instance.card03.color.g, hUDManager.instance.card03.color.b, 1f);
                 hUDManager.instance.keyCard03Text.color = new Color(hUDManager.instance.keyCard03Text.color.r, hUDManager.instance.keyCard03Text.color.g, hUDManager.instance.keyCard03Text.color.b, 1f);
-                gameManager.instance.keyCard[2] = !gameManager.instance.keyCard[2];
+                gameManager.instance.keyCard[2] = true;
             }
         }
     }
